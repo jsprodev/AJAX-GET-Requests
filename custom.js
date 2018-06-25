4552e2900c94f3e23f0de382cb57fe2f901d8491
hashim <hashim@recruitbpm.com>
2018-06-25 11:17:57 +0500

    implemented filtering on table

--- custom.js
+++ custom.js
@@ -4,6 +4,7 @@ document.querySelector('#jsOnload').addEventListener('click', function () {
   xhr.onload = function() {
     if (xhr.status >= 200 && xhr.status < 400) {
       dataObj = JSON.parse(xhr.responseText);
+      showSearchInput();
       loadDataInTablualrForm(dataObj);
       removeLoader();
     }
@@ -20,6 +21,7 @@ document.querySelector('#jsOnReadyStateChange').addEventListener('click', functi
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       dataObj = JSON.parse(xhr.responseText);
+      showSearchInput();
       loadDataInTablualrForm(dataObj);
       removeLoader();
     }
@@ -38,6 +40,7 @@ document.querySelector('#jsFetch').addEventListener('click', function() {
       if (response.ok) {
         response.json()
         .then(function(dataObj) {
+          showSearchInput();
           loadDataInTablualrForm(dataObj);
           disableButton('jsFetch');
           removeLoader();
@@ -54,7 +57,8 @@ $('#jQueryAjax').one('click', function() {
   addLoader();
   $.ajax('https://jsonplaceholder.typicode.com/comments?postId=4')
     .done(function(data, statusText, xhrObj) {
-      var table = $('<table>').addClass('table table-striped table-bordered table-hover table-dark table-sm');
+      showSearchInput();
+      var table = $('<table>').addClass('table table-striped table-bordered table-hover table-dark table-sm').attr('id', 'dataTable');
       var thead = $('<thead>').addClass('thead-light');
       var tbody = $('<tbody>');
       for (var key in data[0]) {
@@ -83,7 +87,8 @@ $('#jQueryAjax').one('click', function() {
 $('#jQueryLoad').one('click', function() {
   addLoader();
   $('#table').load('https://jsonplaceholder.typicode.com/comments?postId=5', function(responseText, statusText, xhrObj) {
-    var table = $('<table>').addClass('table table-striped table-bordered table-hover table-dark table-sm');
+    showSearchInput();
+    var table = $('<table>').addClass('table table-striped table-bordered table-hover table-dark table-sm').attr('id', 'dataTable');
       var thead = $('<thead>').addClass('thead-light');
       var tbody = $('<tbody>');
       for (var key in JSON.parse(responseText)[0]) {
@@ -106,7 +111,8 @@ $('#jQueryLoad').one('click', function() {
 $('#jQueryGet').one('click', function() {
   addLoader();
   $.get('https://jsonplaceholder.typicode.com/comments?postId=6', function(data, statusText) {
-    var table = $('<table>').addClass('table table-striped table-bordered table-hover table-dark table-sm');
+    showSearchInput();
+    var table = $('<table>').addClass('table table-striped table-bordered table-hover table-dark table-sm').attr('id', 'dataTable');
     var thead = $('<thead>').addClass('thead-light');
     var tbody = $('<tbody>');
     for (var key in data[0]) {
@@ -128,7 +134,9 @@ $('#jQueryGet').one('click', function() {
 // get data from xhrObject in tabular form
 function loadDataInTablualrForm(dataObj) {
   var html = '';
-  html += '<table class="table table-striped table-bordered table-hover table-dark table-sm"><thead class="thead-light"><tr>';
+  html += ` <table class="table table-striped table-bordered table-hover table-dark table-sm" id="dataTable">
+              <thead class="thead-light">
+                <tr> `;
   for (var key in dataObj[0]) {
   if (dataObj[0].hasOwnProperty(key)) {
     html += '<th>'+key+'</th>'
@@ -158,4 +166,32 @@ function addLoader() {
 function removeLoader() {
   var loader = document.getElementById('loader');
   loader.parentNode.removeChild(loader);
+}
+
+// show search input
+function showSearchInput() {
+  document.querySelector('#searchDiv').classList.remove('d-none');
+}
+
+// search in table
+function searchTable() {
+  var input, filter, found, table, tr, td, i, j;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("dataTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td");
+      for (j = 0; j < td.length; j++) {
+          if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+              found = true;
+          }
+      }
+      if (found) {
+          tr[i].style.display = "";
+          found = false;
+      } else {
+          tr[i].style.display = "none";
+      }
+  }
 }
